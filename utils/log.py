6c4f21436d74b8eb127123c32ef5fd99ef69b2a8
mlyde <mlyder@163.com>
2025-06-05 14:39:47 +0800

    细分模块日志

--- utils/log.py
+++ utils/log.py
@@ -4,30 +4,33 @@
 
 from pathlib import Path
 import logging
+from logging import Formatter
 from logging.handlers import RotatingFileHandler
 from core.static_config import static_config
 
-
-if static_config["debug"]:
-    LOG_LEVEL = logging.DEBUG
-else:
-    LOG_LEVEL = logging.WARNING
+OTHER_LEVEL = logging.WARNING
+bot_level = static_config.get("level")
+BOT_LEVEL = logging._nameToLevel.get(bot_level)
 
 
-def setup_logging(dir: str = "./log", level = LOG_LEVEL, max_bytes = 64*1024*1024):
-    """全局日志配置中心"""
+def getLoggerConfig(filename: str, level=logging.DEBUG, max_bytes=32*1024*1024, backupCount=1) -> dict:
+    """生成 logger 配置"""
+    return {
+        "level": level,
+        "handlers": [
+            RotatingFileHandler(
+                filename=filename,
+                maxBytes=max_bytes,
+                backupCount=backupCount,
+                encoding="utf-8"
+            )
+        ]
+    }
 
-    # 创建日志目录
-    files_path = Path(dir)
-    files_path.mkdir(exist_ok=True)
+def rootLogging(file_path, log_format: str, date_format: str, level=logging.DEBUG, max_bytes=32*1024*1024, backupCount=8):
+    """根日志配置 (全局继承)"""
 
-    # 基础格式配置
-    log_format = "%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s"
-    date_format = "%Y-%m-%d %H:%M:%S"
-    formatter = logging.Formatter(fmt=log_format, datefmt=date_format)
-
-    # 两种方式操作根记录器
-    # # 根日志配置 (全局继承)
+    # ================ 方法 1 ================
     # root_logger = logging.getLogger()
     # root_logger.setLevel(log_level)
 
@@ -44,43 +47,33 @@ def setup_logging(dir: str = "./log", level = LOG_LEVEL, max_bytes = 64*1024*102
     #     handler.setFormatter(formatter)
     #     root_logger.addHandler(handler)
 
-    # ================ 总日志 ================
+    # ================ 方法 2 ================
     # 手动配置根记录器配置, basicConfig 可防止重复配置 (仅在根记录器无处理器时生效)
     logging.basicConfig(
         format=log_format,
         datefmt=date_format,
         level=level,
         handlers=[
-            logging.StreamHandler(),             # 日志输出到终端
-            RotatingFileHandler(files_path / "_all.log", maxBytes=max_bytes, backupCount=8, encoding="utf-8"),  # 日志写入到文件
+            logging.StreamHandler(),# 日志输出到终端
+            RotatingFileHandler(    # 日志写入到文件
+                file_path / "_all.log",
+                maxBytes=max_bytes,
+                backupCount=backupCount,
+                encoding="utf-8"
+            ),
         ]
     )
 
-    # ================ 单独文件日志 ================
-    logger_bot ={
-        "level": logging.DEBUG,
-        "handlers": [
-            RotatingFileHandler(
-                filename=files_path / "bot.log",
-                maxBytes=max_bytes, 
-                backupCount=8,
-                encoding="utf-8"
-            )
-        ]
-    }
-    logger_message = {
-        "level": logging.DEBUG,
-        "handlers": [
-            RotatingFileHandler(
-                filename=files_path / "message.log",
-                maxBytes=max_bytes,
-                backupCount=1,
-                encoding="utf-8"
-            )
-        ]
-    }
 
-    # 可设置部分模块另存独立日志
+def fileLogging(files_path, formatter: Formatter, level = logging.DEBUG):
+    """记录不同日志文件"""
+
+    logger_bot     = getLoggerConfig(filename=files_path / "bot.log", level=level)
+    logger_message = getLoggerConfig(filename=files_path / "message.log", level=level)
+    logger_member  = getLoggerConfig(filename=files_path / "member.log", level=level)
+    logger_error   = getLoggerConfig(filename=files_path / "error.log", level=level)
+
+    # 不同模块独立日志
     module_loggers = {
         "main": logger_bot,
         "utils.check_contents": logger_bot,
@@ -97,11 +90,11 @@ def setup_logging(dir: str = "./log", level = LOG_LEVEL, max_bytes = 64*1024*102
         "core.static_config": logger_bot,
         "handle.callback_handle": logger_bot,
         "handle.command_handle": logger_bot,
-        "handle.error_handle": logger_bot,
-        "handle.member_handle": logger_bot,
+        "handle.error_handle": logger_error,
+        "handle.member_handle": logger_member,
         "handle.message_handle": logger_message,
         "handle.other_handle": logger_bot,
-        "handle.reaction_handle": logger_bot,
+        "handle.reaction_handle": logger_message,
     }
 
     for logger_name, config in module_loggers.items():
@@ -111,8 +104,25 @@ def setup_logging(dir: str = "./log", level = LOG_LEVEL, max_bytes = 64*1024*102
             handler.setFormatter(formatter)
             logger.addHandler(handler)
 
+def setup_logging(dir: str = "./log", level = OTHER_LEVEL, max_bytes = 32*1024*1024):
+    """全局日志配置"""
+
+    # 创建日志目录
+    files_path = Path(dir)
+    files_path.mkdir(exist_ok=True)
+
+    # 配置基础格式
+    log_format = "%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    formatter = Formatter(fmt=log_format, datefmt=date_format)
+
+    # 配置日志记录器
+    rootLogging(files_path, log_format, date_format, level=level, max_bytes=max_bytes)
+    fileLogging(files_path, formatter, level=BOT_LEVEL)
+
     logger = logging.getLogger(__name__)
     logger.debug("log initialization complete.")
+
 
 if __name__ == "__main__":
 
